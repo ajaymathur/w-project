0330dd016c7f2b963359a04a6878b4c47a06c4e4
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2020-03-03 19:01:36 +1100

    limit the options to 10 and clean up:

--- src/commands/open.ts
+++ src/commands/open.ts
@@ -15,31 +15,19 @@ export default async function open({
   conf
 }: Open) {
   const workspaces = conf.get('workspaces');
-  console.log({ workspaces })
   const workspaceStruct: string[] = (await Promise.all([...workspaces.map(async workspacePath => {
     const projects: string[] = await fs.readDirectory(workspacePath);
     return projects.map(project => `${project}: ${path.resolve(workspacePath, project)}`)
   })])).flat();
 
-  console.log({workspaceStruct});
-
-  // const prompt = new Input({
-  //   name: 'username',
-  //   message: 'What is your username?'
-  // });
-  
-  // prompt.run()
-  //   .then(answer => console.log('Username:', answer))
-  //   .catch(console.error);
-
   const t: { projectToOpen: string } = await prompt({
     type: 'autocomplete',
     name: 'projectToOpen',
+    // @ts-ignore enquirer is missing the limit in its args type
+    limit: 10,
     message: 'Pick the project you are going to work on:',
     choices: [...workspaceStruct]
   });
-
-  console.log(t.projectToOpen.split('-')[1].trim())
 
   await execPromise(`code ${t.projectToOpen.split(':')[1].trim()}`)
 }
